4f10bfc36f03c29d903b2aea0470943f5d499f1e
ropsupops <99011693+ropsupops@users.noreply.github.com>
2022-02-10 10:12:58 +0200

    Tiedostojen ja alustavan pohjan luonti
    Luotiin tarvittavat tiedostot, joissa alustavaa pohjaa sivulle

---
+++ header.php
@@ -0,0 +1,7 @@
+<?php
+echo "
+Moi -
+Hei -
+Heipsuli Hei -
+";
+?>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-Include</title>
+</head>
+<body>
+    <div class="header">
+        <?php
+        include 'header.php';
+        ?>
+    </div>
+    <div class="main">
+        <h1>PHP-Include</h1>
+        <p>Testaillaan miten PHP-Include toimii</p>
+    </div>
+    <div class="footer">
+        <?php
+        include 'footer.php';
+        ?>
+    </div>
+</body>
+</html>
